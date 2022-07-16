92c899c0ecd24d7e435429b495f911a75edda5b1
Johan <jpettersen50@gmail.com>
2022-07-16 22:43:54 +0200

    done for the day

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const libraryContainer = document.querySelector('.libraryContainer');
 
 let myLibrary = [];
 
@@ -12,23 +13,37 @@ function Book(title, author, pages, read) {
 
 }
 
-Book.prototype.info = function() {
+Book.prototype.info = function () {
     return title, author, pages, read;
 }
 
 
 
 function addBookToLibrary(title, author, pages, read) {
-const newBook = Object.create(Book);
-newBook.title = title;
-newBook.author = author;
-newBook.pages = pages;
-newBook.read = read;
-myLibrary.push(newBook);
+    const newBook = Object.create(Book);
+    newBook.title = title;
+    newBook.author = author;
+    newBook.pages = pages;
+    newBook.read = read;
+    myLibrary.push(newBook);
 }
+
+function createCard {
+    
+}
+
 
 function displayBooks() {
-    myLibrary.forEach(console.log(title, author, pages, read));
+    for (let i = 0; i < myLibrary.length; i++) {
+
+        libraryContainer.appendChild(card);
+        card.innerHTML = '<p> Title: </p>' + this.title + '<p> Author: </p>' + this.author + '<p> Pages: </p>' + this.pages + '<p> Read: </p> ' + this.read;
+
+
+    }
 
 
 }
+
+addBookToLibrary('Lord Of The Rings', 'J.R.R Tolkien', '3000', true);
+displayBooks();
