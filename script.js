8bc01c1fd08a8eb517ae725debbf1de426bb7247
Johan <jpettersen50@gmail.com>
2022-07-17 17:34:19 +0200

    add remove book function

--- script.js
+++ script.js
@@ -40,26 +40,73 @@ addBookToLibrary('Lord Of The Rings 3', 'J.R.R Tolkien 3', '3050', true);
 addBookToLibrary('Lord Of The Rings 4', 'J.R.R Tolkien 4', '3200', true);
 
 
-function displayBooks() {
+/* const cardContent = '<div class="card"><p> Title: ' + myLibrary[i].title + '</p><p> Author: ' + myLibrary[i].author + '</p><p> Pages: ' + myLibrary[i].pages + '</p><p> Read: ' + myLibrary[i].read + '</p></div> ';
+ */
+
+/* function displayBooks() {
 
 for(let i = myLibrary.length - 1; i >= 0; i--) {
     console.log(myLibrary[i]);
     libraryContainer.innerHTML += '<div class="card"><p> Title: ' + myLibrary[i].title + '</p><p> Author: ' + myLibrary[i].author + '</p><p> Pages: ' + myLibrary[i].pages + '</p><p> Read: ' + myLibrary[i].read + '</p></div> ';
+const para = document.createElement('div');
+para.innerText = 'Title: ' + myLibrary[i].title + 'Author: ' + myLibrary[i].author + 'Pages: ' + myLibrary[i].pages + 'Read: ' + myLibrary[i].read;
+libraryContainer.appendChild(para);
+
 }
-
-
-
 }
+ */
 
+myLibrary.forEach(function (value, i) {
+
+    let card = document.createElement('div');
+    card.className = 'card';
+    card.id = 'id' + i;
+    let name = document.createTextNode('Title: ' + value.title);
+    console.log(value);
+    card.appendChild(name);
+
+    let author = document.createTextNode('Author: ' + value.author);
+    card.appendChild(author);
+
+    let pages = document.createTextNode('Pages: ' + value.pages);
+    card.appendChild(pages);
+
+    let read = document.createTextNode('Read: ' + value.read);
+    card.appendChild(read);
+
+    libraryContainer.appendChild(card);
+
+    let btn = document.createElement('button');
+    btn.className = 'rmBookBtn';
+    btn.textContent = 'Remove Book';
+    btn.id = 'rmbtn' + i;
+    card.appendChild(btn);
+
+    btn.addEventListener('click', () => {
+        const index = myLibrary.indexOf(value.title)
+        console.log(index);
+       let removed = myLibrary.splice(index, 1);
+        console.log(removed);
+        
+    });
+
+
+
+});
+
+
+
+
+
+/* 
 function newBookForm() {
     document.querySelector('#newBookForm').style.display = 'flex';
 
-/* container.innerHTML =  */
+ container.innerHTML =  
 }
+ */
 
-
-displayBooks();
-newBookBtn.addEventListener('click', newBookForm);
+/* newBookBtn.addEventListener('click', newBookForm);
 
 document.querySelector('form').onsubmit = function(){
     
@@ -72,6 +119,6 @@ document.querySelector('form').onsubmit = function(){
         console.log(myLibrary);
 displayBooks()
 
-}
+} */
 
 
